5d69c65e94d302fc0c47c71f4b7ce708dbae2d03
InvalcreInc <152579264+InvalcreInc@users.noreply.github.com>
2024-10-21 10:26:44 +0300

    Updated DateTimeExt: added timeZoneOffset

--- lib/presentation/core/models/extentions/date_time.dart
+++ lib/presentation/core/models/extentions/date_time.dart
@@ -3,6 +3,9 @@ import 'package:sss/presentation/core/models/extentions/strings.dart';
 extension DateTimeExt on DateTime {
   /// Returns [DateTime] in format 'dd.mm.yy hh:mm'
   String formatRU() {
-    return '${'$day'.padNumber()}.${'$month'.padNumber()}.${'$year'.substring(2)} ${'$hour'.padNumber()}:${'$minute'.padNumber()}';
+    
+    final tz =
+        '${timeZoneOffset.isNegative ? '' : '+'}${timeZoneOffset.inHours}';
+    return '${'$day'.padNumber()}.${'$month'.padNumber()}.${'$year'.substring(2)} ${'$hour'.padNumber()}:${'$minute'.padNumber()} $tz';
   }
 }
